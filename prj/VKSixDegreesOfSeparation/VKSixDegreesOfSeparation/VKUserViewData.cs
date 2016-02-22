21260618b430c1da881ee6b76f5658efab56de99
Savagyan <sery.serezha2017@yandex.ru>
2016-02-22 04:13:51 +0300

    Design starter pack added
    1. Textboxes, button
    2. Some validation

---
+++ prj/VKSixDegreesOfSeparation/VKSixDegreesOfSeparation/VKUserViewData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKSixDegreesOfSeparation
+{
+    class VKUserViewData : VKUser
+    {
+        public VKUserViewData(string nick) 
+            : base(0)
+        {
+            _nick = nick;
+        }
+
+        public VKUserViewData()
+            : base(0)
+        {
+            _nick = "";
+        }
+
+        public bool validateNick()
+        {
+            if (_nick == "")
+            {
+                return false;
+            }
+
+            //get user id from net
+            //или сделать загрузку инфы сверху этого класса. 
+            return true;
+        }
+
+        protected string _nick;
+    }
+}
